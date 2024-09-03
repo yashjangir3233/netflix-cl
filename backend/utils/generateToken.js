1ae62b7d75e2603dbb6653d8f240e4de535f83a1
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-09-03 13:22:23 +0530

    cookie fix 3

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt-netflix", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
-		sameSite: "none", // CSRF attacks cross-site request forgery attacks
+		sameSite: "None", // CSRF attacks cross-site request forgery attacks
 		secure: true,
 		domain:"netflix-cl-et9n.vercel.app"
 	});
