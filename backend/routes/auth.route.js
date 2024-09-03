d241304db62925393eab812c55618069dd0f09b4
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-09-03 10:38:11 +0530

    backend redploy

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { allUser, authCheck, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.get("/authCheck", protectRoute, authCheck);
+router.get('/all',allUser);
+
+export default router;
